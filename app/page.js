0921324c804801f1d851bbd0375606330874754e
Hitesh Nathani <hitesh.nathani@groovyweb.co>
2025-02-07 10:47:57 +0530

    Update app file

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>app/page.js</code>.
+            Get started by editing <code>app/page.js</code>. Hello world
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
